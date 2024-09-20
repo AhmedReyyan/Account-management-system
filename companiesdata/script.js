4b01bdfbd0bfa9ef6b38579c1f2ff72e2fceacaf
Ahmed Reyyan <reyyanahmed085@gmail.com>
2024-09-20 11:25:22 +0500

    worked on login page and trying to set companies model

--- companiesdata/script.js
+++ companiesdata/script.js
@@ -1,18 +1,5 @@
-let companies= [company1,company2,company3]
-
-
-let company1 = {
-    owner:'Ahmed',
-    name:"toyota",
-    password:'1234',
-    adminName:'Ahmed11',
-    revenuw:300000,
-    users:[{name:'ali',age:'20',role:'employee',pay:40000},{name:'raza',age:23,role:'employee',pay:50000},]
-    
-}
-let company2 = {
-
-}
-let company3 = {
-
+let website = {
+    Administrator : 'ahmedreyyan',
+    password  : 'ahmedreyyan11',
+    companies : {100:{},101:{},103:{}}
 }
--- login.js
+++ login.js
@@ -7,9 +7,13 @@ let companyselects = document.querySelector('.companyroleselect')
 let loginbtn = document.getElementById('loginbtn');
 let loginHeading = document.getElementById('loginheading');
 let loginmain = document.querySelector('.loginmain');
+let logininputsdiv = document.querySelector('.logininputs')
+
 
 let adminUserName = 'ahmedreyyan'
 let adminUserPassword = 'ahmedreyyan11'
+
+let login_role_state = null;
 
 loginbtn.addEventListener('click',()=>{
     
@@ -28,6 +32,10 @@ selectRoleBtn.addEventListener('click',()=>{
 
 
 window.onload(selectRole())
+
+
+
+
 
 function selectRole(){
     let divpage = document.createElement('div');
@@ -50,11 +58,57 @@ function selectRole(){
     let selected  = document.querySelectorAll('.selectionbtn');
     selected.forEach(item=>{
         item.addEventListener('click',()=>{
-        loginHeading.innerHTML = item.innerHTML
-       document.body.removeChild(divpage);
-        if (item.innerHTML == 'Company') {
-            Companylogin()
-        }
+
+loginHeading.innerHTML = item.innerHTML
+            if (login_role_state==null) {
+                
+                login_role_state = item.innerHTML
+                if (login_role_state== 'Company') {
+                    usernameheading.innerHTML = "User Name"
+                    
+                    Companylogin();
+                    document.body.removeChild(divpage)
+
+                }else if (login_role_state== 'Admin') {
+                    document.body.removeChild(divpage)
+                    usernameheading.innerHTML = 'Administrator Id';
+                }
+                
+
+            }else
+            if (login_role_state== "Admin" && item.innerHTML=="Company") {
+
+                login_role_state= 'Company'
+                usernameheading.innerHTML = "User Name"
+                Companylogin()
+                document.body.removeChild(divpage)
+
+            }else
+            if (login_role_state== "Company" && item.innerHTML=='Admin') {
+                login_role_state = 'Admin'
+                usernameheading.innerHTML = "Administrator Id"
+                logininputsdiv.removeChild(logininputsdiv.querySelector('.companyfield'))
+                loginmain.removeChild(loginmain.querySelector('.companyroleselect'))
+                document.body.removeChild(divpage)
+
+            }else if(login_role_state=='Company' && item.innerHTML == 'Company'){
+                      document.body.removeChild(divpage)
+            }else if(login_role_state=='Admin' && item.innerHTML == 'Admin'){
+                document.body.removeChild(divpage)
+      }
+
+        //     if (login_role_state==null) {
+              
+        //           loginHeading.innerHTML = item.innerHTML
+        //           document.body.removeChild(divpage);
+        // if (loginHeading.innerHTML == 'Company'  || login_role_state == 'Admin') {
+        //     login_role_state = loginHeading.innerHTML
+        //     Companylogin()
+            
+        // }
+        //      }
+      
+       
     })
     })
 
@@ -64,17 +118,16 @@ function selectRole(){
 
 
 
-
 function Companylogin(){
  
-
+companyId();
     let companyBtnDiv = document.createElement('div');
 companyBtnDiv.className = 'companyroleselect';
 let companyadmin =  document.createElement('button');
-companyadmin.className = 'companyrolebtn';
-companyadmin.innerHTML = 'Company Admin';
+companyadmin.className = 'companyrolebtn Adminbtnc';
+companyadmin.innerHTML = 'Company Adminbtn';
 let companyuser =  document.createElement('button');
-companyuser.className = 'companyrolebtn';
+companyuser.className = 'companyrolebtn Userbtnc';
 companyuser.innerHTML = 'Company User'
 loginmain.appendChild(companyBtnDiv)
 companyBtnDiv.appendChild(companyadmin)
@@ -85,3 +138,31 @@ companyBtnDiv.appendChild(companyuser)
     
 
 }
+
+function companyId(){
+
+    let companyfield = document.createElement('div');
+    companyfield.className = 'field companyfield';
+    let companyfieldheading = document.createElement('h4');
+    companyfieldheading.id = 'passwordheading';
+    companyfieldheading.innerHTML = "Company :"
+    let companyfieldinput = document.createElement('input');
+    companyfieldinput.className = "companyinput";
+    companyfieldinput.type = "text";
+    companyfieldinput.required = true
+    companyfieldinput.placeholder = "Entercompany id";
+    companyfieldinput.className = "input";
+    companyfieldinput.id = "companyId";
+
+
+    logininputsdiv.appendChild(companyfield);
+    companyfield.appendChild(companyfieldheading)
+    companyfield.appendChild(companyfieldinput)
+}
+
+
+let companyRoleBtns = document.querySelector('.companyroleselect');
+companyRoleBtns.addEventListener('click',()=>{
+    console.log("hello");
+    
+})
