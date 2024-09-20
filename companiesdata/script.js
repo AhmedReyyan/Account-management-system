6cf79b63528b92f50a676dacb6341def41fb47f0
Ahmed Reyyan <reyyanahmed085@gmail.com>
2024-09-20 12:45:44 +0500

    setting companies with local storage

--- companiesdata/script.js
+++ companiesdata/script.js
@@ -1,5 +1,44 @@
+let newCompany = document.querySelector('.createcompany');
+
 let website = {
     Administrator : 'ahmedreyyan',
     password  : 'ahmedreyyan11',
-    companies : {100:{},101:{},103:{}}
+    companies : [{},{},{}]
+}
+
+
+
+newCompany.addEventListener('click',()=>{
+    console.log('click');
+   
+      newfunction();
+
+   
+})
+
+function newfunction(){
+let elem = new Object();
+console.log(elem);
+
+website.companies.push(elem)
+storedata()
+let storeData = localStorage.setItem('website',JSON.stringify(website));
+}
+
+
+
+
+
+function newCompanyDetail(){
+     let datadiv = document.createElement('div')
+
+
+
+}
+
+function storedata() {
+    
+let gettingdata = JSON.parse(localStorage.getItem('website'))
+console.log(gettingdata);
+
 }
